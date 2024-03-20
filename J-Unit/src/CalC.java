363125b23d5009f4a18d47c9c50de0a1785c6323
Admin <Admin@192.168.1.3>
2024-03-20 23:57:12 +0530

    by jj

---
+++ J-Unit/src/CalC.java
@@ -0,0 +1,21 @@
+
+public class CalC {
+
+	public int sum (int a,int b)
+	{
+		int c=a+b;
+		
+		return c;
+	}
+	
+	public int sub (int a,int b)
+	{
+		int c=a-b;
+		
+		return c;
+	}
+	
+	
+	
+	
+}
